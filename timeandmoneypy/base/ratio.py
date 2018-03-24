83a0ab6fcb32d83e62008e179edacbeca32faf85
Ilya Gotfyrd <igotfryd@onshift.com>
2018-03-24 12:28:44 -0400

    added untested IntervalLimit

--- timeandmoneypy/base/ratio.py
+++ timeandmoneypy/base/ratio.py
@@ -1,5 +1,19 @@
 from decimal import *
-
+""" 
+ * Copyright (c) 2004 Domain Language, Inc. (http://domainlanguage.com) This
+ * free software is distributed under the "MIT" licence. See file licence.txt.
+ * For more information, see http://timeandmoney.sourceforge.net.
+ 
+ * Ratio represents the unitless division of two quantities of the same type.
+ * The key to its usefulness is that it defers the calculation of a decimal
+ * value for the ratio. An object which has responsibility for the two values in
+ * the ratio and understands their quantities can create the ratio, which can
+ * then be used by any client in a unitless form, so that the client is not
+ * required to understand the units of the quantity. At the same time, this
+ * gives control of the precision and rounding rules to the client, when the
+ * time comes to compute a decimal value for the ratio. The client typically has
+ * the responsibilities that enable an appropriate choice of these parameters.
+ """
 
 class Ratio:
     def __init__(self, numerator, denominator):
---
+++ timeandmoneypy/intervals/interval_limit.py
@@ -0,0 +1,61 @@
+class IntervalLimit():
+    def __init__(self, closed, lower, value):
+        self._closed = closed
+        self._lower = lower
+        self._value = value
+
+    @staticmethod
+    def upper(closed, value):
+        return IntervalLimit(closed, False, value)
+
+    @staticmethod
+    def lower(closed, value):
+        return IntervalLimit(closed, True, value)
+
+    def is_lower(self):
+        return self._lower
+
+    def is_upper(self):
+        return not self._lower
+
+    def is_closed(self):
+        return self._closed
+
+    def is_opened(self):
+        return not self._closed
+
+    def get_value(self):
+        return self._value
+
+    def compare_to(self, other):
+        if other is not None and isinstance(other, IntervalLimit):
+            if other.get_value() == self._value:
+                return 0
+            if self._value is None:
+                return -1 if self._lower else 1
+            if other.get_value() is None
+                return 1 if other.is_lower() else -1
+            return -1 if self._value < other.get_value() else \
+                    1 if self._value > other.get_value() else 0
+        raise TypeError('Expected IntervalLimit but was of type {}'.format(other.__class__.__name__) )
+
+    def __cmp__(self, other):
+        return self.compare_to(other)
+
+    def __eq__(self, other):
+        return self.compare_to(other) == 0
+
+    def __ne__(self, other):
+        return self.compare_to(other) != 0
+
+    def __lt__(self, other):
+        return self.compare_to(other) == -1
+
+    def __gt__(self, other):
+        return self.compare_to(other) == 1
+
+    def __ge__(self, other):
+        return self.compare_to(other) in [1,0]
+
+    def __le__(self, other):
+        return self.compare_to(other) in [-1,0]
